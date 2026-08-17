2ce2b0b5fb17174e44e26815099955b6f7bb16bd
tomnGithub <165872442+tomnGithub@users.noreply.github.com>
2026-08-16 20:58:09 -0700

    Update Settings.cs

--- CELLRecordForwarder/Settings.cs
+++ CELLRecordForwarder/Settings.cs
@@ -6,5 +6,5 @@ namespace CELLRecordForwarder;
 public record Settings
 {
     [SettingName("Lighting Mod Plugin")]
-    public ModKey LightingModPlugin { get; set; } = ModKey.FromFileName("Lux.esp");
+    public List<ModKey> LightingModPlugin = new List<ModKey>();
 }
