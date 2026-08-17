9fb250700f52a69fe514a225e6ad1b094b8b71ad
tomnGithub <165872442+tomnGithub@users.noreply.github.com>
2026-08-16 20:59:03 -0700

    Update Settings.cs

--- CELLRecordForwarder/Settings.cs
+++ CELLRecordForwarder/Settings.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; // Add this namespace
 using Mutagen.Bethesda.Plugins;
+using System.ComponentModel;
 using Mutagen.Bethesda.WPF.Reflection.Attributes;
-
+using Synthesis.Bethesda.Commands;
 namespace CELLRecordForwarder;
 
 public record Settings
