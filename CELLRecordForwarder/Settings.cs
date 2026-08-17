d461394d0d1dccbfb927c85c23424c576d19db95
tomnGithub <165872442+tomnGithub@users.noreply.github.com>
2026-08-17 13:48:07 -0700

    Add files via upload

--- CELLRecordForwarder/Settings.cs
+++ CELLRecordForwarder/Settings.cs
@@ -2,19 +2,13 @@ using System.Collections.Generic;
 using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.WPF.Reflection.Attributes;
 
-namespace CELLRecordForwarder;
+namespace WeaponSpeedReachForwarder;
 
 public record Settings
 {
-    [SettingName("Enable Lighting")]
-    public bool EnableLighting = true;
+    [SettingName("Use Reqtificator Settings")]
+    public bool UseReqtificatorSettings = false;
 
-    [SettingName("Lighting Mod Plugin")]
-    public List<ModKey> LightingModPlugin = new List<ModKey>();
-
-    [SettingName("Enable Water")]
-    public bool EnableWater = true;
-
-    [SettingName("Water Mod Plugin")]
-    public List<ModKey> WaterModPlugin = new List<ModKey>();
+    [SettingName("Weapon Mod Plugin")]
+    public List<ModKey> WeaponModPlugin = new List<ModKey>();
 }
